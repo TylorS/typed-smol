d0aa37da4af84338846b727c602e153b194f7794
Tylor Steinberger <tylor@tradester.com>
2026-08-23 14:05:11 -0400

    fix: types are broken, will fix soon !

--- examples/fullstack/src/server.ts
+++ examples/fullstack/src/server.ts
@@ -7,6 +7,7 @@ import * as Http from "node:http";
 import type { ViteDevServer } from "vite";
 import { routes } from "./app.js";
 import { makeHost } from "./host.js";
+import { Ids } from "@typed/id";
 
 export interface ServerOptions {
     readonly port: number;
@@ -20,6 +21,7 @@ const HttpRoutes = HttpRouter.use(ssrForHttp(routes)).pipe(
 
 export const runServer = (options: ServerOptions) =>
     makeHost(HttpRoutes, options.vite).pipe(
+        Layer.provide(Ids.Default),
         Layer.provide(NodeHttpServer.layer(Http.createServer, options)),
         Layer.launch,
         NodeRuntime.runMain,
